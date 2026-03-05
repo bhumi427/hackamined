cd5fd64275d13e1d5490103a072b4c303bbccca6
23f2001336 <23f2001336@ds.study.iitm.ac.in>
2026-03-06 04:23:36 +0530

    sustainable api calls

--- backend/image_generator.py
+++ backend/image_generator.py
@@ -1,7 +1,27 @@
-import requests
+# import requests
 
-def get_scene_image(query):
 
-    url = f"https://source.unsplash.com/1600x900/?{query}"
+# def get_scene_image(query):
+#     query = query.replace(" ", ",")
 
-    return url
+#     url = f"https://source.unsplash.com/featured/1600x900/?{query}"
+
+#     return url
+
+
+import random
+
+def get_scene_images(query, num_images=3):
+
+    images = []
+
+    for i in range(num_images):
+
+        # random image generator
+        seed = random.randint(1, 1000)
+
+        url = f"https://picsum.photos/seed/{seed}/800/450"
+
+        images.append(url)
+
+    return images
